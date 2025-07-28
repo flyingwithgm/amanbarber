a9f89bdce454dd2528ef1a4adb03bf6bff0e02c0
flyingwithgm <georgenuyekpe@gmail.com>
2025-07-28 01:18:27 +0000

    Update booking-form.js

--- public/js/booking-form.js
+++ public/js/booking-form.js
@@ -1,10 +1,22 @@
 import { auth, db } from './firebase.js';
 import { collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
 
-/* Pre-select service from URL */
+const serviceSelect = document.getElementById('service');
+serviceSelect.innerHTML = `
+  <option value="" disabled selected>-- Pick a Service --</option>
+  <option>Regular Haircut – GHS 100</option>
+  <option>Beard Trim – GHS 80</option>
+  <option>Cut + Enhancement – GHS 120</option>
+  <option>Haircut with Texturizer – GHS 180</option>
+  <option>Pixie Cut – GHS 250</option>
+  <option>Extensions – GHS 500</option>
+  <option>Complete Colored Hair – GHS 220</option>
+  <option>Part-Colored Haircut – GHS 180</option>
+`;
+
 const urlParams = new URLSearchParams(location.search);
 const wanted = decodeURIComponent(urlParams.get('service') || '');
-[...document.getElementById('service').options].forEach(opt=>{
+[...serviceSelect.options].forEach(opt=>{
   if (opt.text === wanted) opt.selected = true;
 });
 
@@ -13,7 +25,7 @@ document.getElementById('bookingForm').addEventListener('submit', async e=>{
   const data = {
     uid:     auth.currentUser.uid,
     email:   auth.currentUser.email,
-    service: document.getElementById('service').value,
+    service: serviceSelect.value,
     date:    document.getElementById('date').value,
     time:    document.getElementById('time').value,
     phone:   document.getElementById('phone').value,
